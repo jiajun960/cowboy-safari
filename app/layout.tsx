fa437af7ad76d32a802f30131b9bfc97b65cc923
jiajun960 <1021046253@qq.com>
2025-12-01 18:04:09 +0800

    Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -107,6 +107,16 @@ export default function RootLayout({
             gtag('config', 'G-H2V78YF7VE');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "uenpazr184");
+          `}
+        </Script>
       </head>
       <body className={`font-sans antialiased`}>
         {children}
